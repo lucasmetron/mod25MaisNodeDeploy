2ee99d377a01e11f9c84776d5d581f4a3ea67d44
lucasmetron <lucasmetron@gmail.com>
2021-05-27 13:46:23 -0300

    dotenv

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 
+
+require('dotenv').config();
+
+let port = process.env.PORT
+
 app.get('/', (req, res) => {
     res.send('<h1>Testando Umbler! Hello wolrd :D')
 })
 
 app.listen(3000, () => {
-    console.log('Running server on 3000')
+    console.log(process.env.API_KEY)
+    console.log(`Running server on ${port}`)
 })
